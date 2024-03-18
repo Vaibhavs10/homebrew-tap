7308d3a82aebb4ce347d13543132b3171609a182
Vaibhav Srivastav <vaibhavs10@gmail.com>
2024-03-18 11:58:33 +0100

    up.

--- Formula/llama.cpp.rb
+++ Formula/llama.cpp.rb
@@ -1,41 +1,23 @@
+$version = "b2453"
+
 class LlamaCpp < Formula
  desc "Inference of Meta's LLaMA model in pure C/C++"
  homepage "https://github.com/ggerganov/llama.cpp"
-  url "https://github.com/ggerganov/llama.cpp.git",
-       using: :git,
-       branch: "master"
- version "0.1.0"
+ url "https://github.com/ggerganov/llama.cpp/archive/refs/tags/#$version.zip"
+ version "#$version"
+ sha256 "139136ad43ff05dab5b26d0c3b41fe5ee2b872b01f6ec642fe5ea6a8ccabca67"
  license "MIT"
 
- depends_on "cmake" => :build
- depends_on "python@3.10"
- depends_on "git"
- depends_on "wget"
+def install
+    # Build the project here
+    ENV["LLAMA_METAL_EMBED_LIBRARY"] = "1"
+    system "make"
 
- def install
-    # Create a temporary directory for cloning
-    temp_dir = Dir.mktmpdir
-    system "git", "clone", "https://github.com/ggerganov/llama.cpp", temp_dir
-
-    # Change to the temporary directory
-    Dir.chdir(temp_dir) do
-      # Compile the Metal shader
-      system "xcrun", "-sdk", "macosx", "metal", "ggml-metal.metal", "-o", "ggml-metal.air"
-      system "xcrun", "-sdk", "macosx", "metallib", "ggml-metal.air", "-o", "ggml-metal.metallib"
-
-      # Build the project here
-      system "make"
-
-      # Install the necessary files to the Homebrew installation directory
-      bin.install "main" => "llama"
-    end
-
-    # Clean up the temporary directory
-    FileUtils.rm_rf(temp_dir)
+    # Install the necessary files to the Homebrew installation directory
+    bin.install "main" => "llama"
  end
 
  test do
     system "#{bin}/llama", "--version"
  end
 end
-
