f33f965bf8a5c686ddc0fb2d1a0bf5f53808ed77
Vaibhav Srivastav <vaibhavs10@gmail.com>
2024-03-22 11:49:41 +0100

    rufo it. x2

--- Formula/llama.cpp.rb
+++ Formula/llama.cpp.rb
@@ -3,21 +3,21 @@ $version = "b2491"
 class LlamaCpp < Formula
   desc "Inference of Meta's LLaMA model in pure C/C++"
   homepage "https://github.com/ggerganov/llama.cpp"
+  version "v-#{$version}"
+  # TODO: Find a way to do SHA computation on the fly.
+  sha256
+  "bd085d7bb4616d0affe2dad69c5da069ec0334bdf5333bb52bf4ea678c4e0b89"
+  license "MIT"
 
   depends_on :macos
   on_arm do
     arch = "arm64"
-    url "https://github.com/ggerganov/llama.cpp/releases/download/#$version/llama-b1-bin-macos-#{arch}.zip"
+    url "https://github.com/ggerganov/llama.cpp/releases/download/#{$version}/llama-b1-bin-macos-#{arch}.zip"
   end
   on_intel do
     arch = "x64"
-    url "https://github.com/ggerganov/llama.cpp/releases/download/#$version/llama-b1-bin-macos-#{arch}.zip"
+    url "https://github.com/ggerganov/llama.cpp/releases/download/#{$version}/llama-b1-bin-macos-#{arch}.zip"
   end
-
-  version "#$version"
-  # TODO: Find a way to do SHA computation on the fly.
-  sha256 "bd085d7bb4616d0affe2dad69c5da069ec0334bdf5333bb52bf4ea678c4e0b89"
-  license "MIT"
 
   def install
     # Install the necessary files to the Homebrew installation directory
