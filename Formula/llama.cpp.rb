2e13024fa4ba8646f9d87cdeb8cb69f65542f091
Vaibhav Srivastav <vaibhavs10@gmail.com>
2024-03-22 11:43:24 +0100

    rufo it.

--- Formula/llama.cpp.rb
+++ Formula/llama.cpp.rb
@@ -6,26 +6,26 @@ class LlamaCpp < Formula
 
   depends_on :macos
   on_arm do
-  arch = "arm64"
-  url "https://github.com/ggerganov/llama.cpp/releases/download/#$version/llama-b1-bin-macos-#{arch}.zip"
-end
+    arch = "arm64"
+    url "https://github.com/ggerganov/llama.cpp/releases/download/#$version/llama-b1-bin-macos-#{arch}.zip"
+  end
   on_intel do
-  arch = "x64"
-url "https://github.com/ggerganov/llama.cpp/releases/download/#$version/llama-b1-bin-macos-#{arch}.zip"
-end
+    arch = "x64"
+    url "https://github.com/ggerganov/llama.cpp/releases/download/#$version/llama-b1-bin-macos-#{arch}.zip"
+  end
 
   version "#$version"
-# TODO: Find a way to do SHA computation on the fly.
+  # TODO: Find a way to do SHA computation on the fly.
   sha256 "bd085d7bb4616d0affe2dad69c5da069ec0334bdf5333bb52bf4ea678c4e0b89"
-license "MIT"
+  license "MIT"
 
-def install
-# Install the necessary files to the Homebrew installation directory
-  bin.install "bin/main" => "llama-cli"
-  bin.install "bin/server" => "llama-server"
-end
+  def install
+    # Install the necessary files to the Homebrew installation directory
+    bin.install "bin/main" => "llama-cli"
+    bin.install "bin/server" => "llama-server"
+  end
 
   test do
-  system "#{bin}/llama", "--version"
-end
+    system "#{bin}/llama", "--version"
+  end
 end
