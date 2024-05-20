3273e3b4c12241f2a1b8319e9534affad9fceea5
Vaibhav Srivastav <vaibhavs10@gmail.com>
2024-05-20 17:26:39 +0200

    up

--- Formula/llama.cpp.rb
+++ Formula/llama.cpp.rb
@@ -1,20 +1,21 @@
 class LlamaCpp < Formula
-desc "LLM inference in C/C++"
-homepage "https://github.com/ggerganov/llama.cpp"
-# pull from git tag to get submodules
-url "https://github.com/ggerganov/llama.cpp.git",
-tag:      "b2568",
-revision: "be55134a535f7218c53f39211755b1c7550851b2"
-license "MIT"
+  desc "LLM inference in C/C++"
+  homepage "https://github.com/ggerganov/llama.cpp"
+  # pull from git tag to get submodules
+  url "https://github.com/ggerganov/llama.cpp.git",
+      tag: "b2568",
+      revision: "be55134a535f7218c53f39211755b1c7550851b2"
+  license "MIT"
 
-def install
-system "make", "DLLAMA_FATAL_WARNINGS=ON", "DLLAMA_METAL_EMBED_LIBRARY=ON", "DLLAMA_CURL=ON"
+  def install
+    system "make", "DLLAMA_FATAL_WARNINGS=ON", "DLLAMA_METAL_EMBED_LIBRARY=ON", "DLLAMA_CURL=ON"
 
-bin.install "./main" => "llama-cli"
-bin.install "./server" => "llama-server"
-end
-test do
-    llama_cli_command = ["llama-cli"].join(" ")
+    bin.install "./main" => "llama-cli"
+    bin.install "./server" => "llama-server"
+  end
+
+  test do
+    llama_cli_command = "llama-cli"
     assert_includes shell_output(llama_cli_command), "Log start"
-end
+  end
 end
