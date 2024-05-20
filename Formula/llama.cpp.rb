d5fde21d376c31fedcd07b97c9bd282be0d74be8
Vaibhav Srivastav <vaibhavs10@gmail.com>
2024-05-20 14:22:26 +0200

    up

--- Formula/llama.cpp.rb
+++ Formula/llama.cpp.rb
@@ -1,29 +1,32 @@
-$version = "b2496"
-
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class LlamaCpp < Formula
-  desc "Inference of Meta's LLaMA model in pure C/C++"
-  homepage "https://github.com/ggerganov/llama.cpp"
-  version "v-#{$version}"
-  # TODO: Find a way to do SHA computation on the fly.
-  sha256 "18b73455ba566ca0b6a30877266b1b4f0f55ddff9c319a97c48acd94fe33662f"
-  license "MIT"
-
-  depends_on :macos
-  on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/ggerganov/llama.cpp/releases/download/#{$version}/llama-b1-bin-macos-x64.zip"
-    else
-      url "https://github.com/ggerganov/llama.cpp/releases/download/#{$version}/llama-b1-bin-macos-arm64.zip"
-    end
-  end
-
-  def install
-    # Install the necessary files to the Homebrew installation directory
-    bin.install "bin/main" => "llama-cli"
-    bin.install "bin/server" => "llama-server"
-  end
-
-  test do
-    system "#{bin}/llama", "--version"
-  end
+desc "LLM inference in C/C++"
+homepage "https://github.com/ggerganov/llama.cpp"
+# pull from git tag to get submodules
+url "https://github.com/ggerganov/llama.cpp.git",
+tag:      "b2568",
+revision: "be55134a535f7218c53f39211755b1c7550851b2"
+sha256 "96017b0693d09f5870155e7215c38764fe152f5cd976c79ddd5046f6c5d33753"
+license "MIT"
+depends_on "cmake" => :build
+depends_on "ccache" => :build
+def install
+system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+system "cmake", "--build", "build", "--config", "Release"
+system "cmake", "--install", "build"
+end
+test do
+# `test do` will create, run in and delete a temporary directory.
+#
+# This test will fail and we won't accept that! For Homebrew/homebrew-core
+# this will need to be a test that verifies the functionality of the
+# software. Run the test with `brew test llama.cpp`. Options passed
+# to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+#
+# The installed folder is not in the path, so use the entire path to any
+# executables being tested: `system "#{bin}/program", "do", "something"`.
+system "false"
+end
 end
